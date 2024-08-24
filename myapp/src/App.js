e6bf19dda19258b2a82bd0eec2d6ad66b636c648
ksanu4221 <ksanu4221@gmail.com>
2024-08-25 00:46:40 +0530

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="bg-white dark:bg-slate-800 py-10">
+    	<p class="text-slate-500 dark:text-slate-400">Tailwind supports Dark mode. </p>
+  </div>
+
+  );
+}
+
+export default App;
